2d8cc9d0d173a99f4435696ea805b2925b53fc32
Exequiel Ceasar Navarrete <exequiel.navarrete09@gmail.com>
2017-03-18 16:32:04 +0800

    Hide sidebar when viewport is too small

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
+import { MdSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  @ViewChild(MdSidenav) sidenav: MdSidenav;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(evt: Event) {
+    console.log(evt);
+    if ((evt.target as any).innerWidth < 500) {
+      this.sidenav.close();
+    } else {
+      this.sidenav.open();
+    }
+  }
+
 }
